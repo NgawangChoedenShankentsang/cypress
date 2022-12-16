c20a0fe10883f902d910cf2a004a859138e95169
NgawangChoedenShankentsang <ngawangchoeden@icloud.com>
2022-12-16 15:56:55 +0100

    new-feature-in-homepage

--- webapp/index.php
+++ webapp/index.php
@@ -1,4 +1,17 @@
 <?php $page_title = "Home ★ Productive"; ?>
 <?php require "view/blocks/page_start.php"; ?>
 <h1>Welcome to Productive!</h1>
+<div style="margin-bottom: 2em;">
+    <h3>Here are some of our popular Products</h3>
+</div>
+<table>
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Stock</th>
+		</tr>
+	</thead>
+	<tbody id="active-products-table"></tbody>
+</table>
+<script src="controller/active-products.js"></script>
 <?php require "view/blocks/page_end.php"; ?>
