4e14ab1f695c05a3df2be0bf7931a743949e4c1f
NgawangChoedenShankentsang <ngawangchoeden@icloud.com>
2022-12-19 11:57:57 +0100

    index change

--- webapp/index.php
+++ webapp/index.php
@@ -1,17 +1,19 @@
 <?php $page_title = "Home ★ Productive"; ?>
 <?php require "view/blocks/page_start.php"; ?>
 <h1>Welcome to Productive!</h1>
-<div style="margin-bottom: 2em;">
-    <h3>Here are some of our popular Products</h3>
-</div>
-<table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>Stock</th>
-		</tr>
-	</thead>
-	<tbody id="active-products-table"></tbody>
-</table>
-<script src="controller/active-products.js"></script>
 <?php require "view/blocks/page_end.php"; ?>
+<script>
+class UserService {
+	window.fizzbuzz = function (num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else if (num % 3 === 0) {
+    return "Fizz";
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  } else {
+    return num;
+  }
+}
+  }
+</script>
