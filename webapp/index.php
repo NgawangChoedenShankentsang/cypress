037274866a8c67ece205327a9c5081184db535a8
NgawangChoedenShankentsang <ngawangchoeden@icloud.com>
2023-01-04 13:37:15 +0100

    new feature

--- webapp/index.php
+++ webapp/index.php
@@ -1,19 +1,16 @@
 <?php $page_title = "Home ★ Productive"; ?>
 <?php require "view/blocks/page_start.php"; ?>
 <h1>Welcome to Productive!</h1>
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Stock</th>
+    </tr>
+  </thead>
+  <tbody id="active-products-table">
+  </tbody>
+</table>
+
+<script src="controller/active-products.js"></script>
 <?php require "view/blocks/page_end.php"; ?>
-<script>
-class UserService {
-	window.fizzbuzz = function (num) {
-  if (num % 3 === 0 && num % 5 === 0) {
-    return "FizzBuzz";
-  } else if (num % 3 === 0) {
-    return "Fizz";
-  } else if (num % 5 === 0) {
-    return "Buzz";
-  } else {
-    return num;
-  }
-}
-  }
-</script>
